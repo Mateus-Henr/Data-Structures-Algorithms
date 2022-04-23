cf3ccffad49c1acfe0cb6b3f65e87c483397e638
Mateus Henrique <mateush.study@gmail.com>
2022-04-23 02:01:01 -0300

    Improve linear search theory

--- LinearSearchAlgorithm/src/tutorial/Main.java
+++ LinearSearchAlgorithm/src/tutorial/Main.java
@@ -11,11 +11,25 @@ package tutorial;
     Search algorithm
     • A search algorithm will find a value in a data structure.
 
+    Example
+
+    Search value = 1
+
+    Step 1
+        [20]                                     End = 1
+        [20] [35]                                End = 2
+        [20] [35] [-15]                          End = 3
+        [20] [35] [-15] [7]                      End = 4
+        [20] [35] [-15] [7] [55]                 End = 5
+        [20] [35] [-15] [7] [55] [1]             End = 6
+
 
     Final conclusion
     • A simple iteration over a data structure.
     • O(n).
  */
+
+import java.util.Arrays;
 
 public class Main
 {
@@ -24,15 +38,20 @@ public class Main
         int[] intArray = {20, 35, -15, 7, 55, 1, -22};
 
         System.out.println("Looking for -15 -> Pos = " + linearSearch(intArray, -15));
-        System.out.println("Looking for -15 -> Pos = " + linearSearch(intArray, 1));
-        System.out.println("Looking for -15 -> Pos = " + linearSearch(intArray, 999));
-        System.out.println("Looking for -15 -> Pos = " + linearSearch(intArray, -22));
+        System.out.println();
+        System.out.println("Looking for 1 -> Pos = " + linearSearch(intArray, 1));
+        System.out.println();
+        System.out.println("Looking for 999 -> Pos = " + linearSearch(intArray, 999));
+        System.out.println();
+        System.out.println("Looking for -22 -> Pos = " + linearSearch(intArray, -22));
     }
 
     public static int linearSearch(int[] input, int value)
     {
         for (int i = 0; i < input.length; i++)
         {
+            printArray(input, i + 1);
+
             if (input[i] == value)
             {
                 return i;
@@ -40,6 +59,16 @@ public class Main
         }
 
         return -1;
+    }
+
+    private static void printArray(int[] array, int end)
+    {
+        for (int i = 0; i < end; i++)
+        {
+            System.out.print("[" + array[i] + "] ");
+        }
+
+        System.out.println();
     }
 
 }
